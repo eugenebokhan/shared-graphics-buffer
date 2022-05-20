da623c3b7a85052c5146d56cb10e1923ee113076
eugene <eugenebokhan@icloud.com>
2022-05-20 13:21:50 +0100

    add CGImage and UIImage extensions

--- Sources/SharedGraphicsBuffer/Extensions/CoreGraphics/CGContext+GraphicsDataProvider.swift
+++ Sources/SharedGraphicsBuffer/Extensions/CoreGraphics/CGContext+GraphicsDataProvider.swift
@@ -1,4 +1,5 @@
 import CoreGraphics
+import Foundation
 
 extension CGContext: GraphicsDataProvider {
     public func graphicsData() throws -> GraphicsData {
@@ -8,6 +9,24 @@ extension CGContext: GraphicsDataProvider {
             width: UInt(self.width),
             height: UInt(self.height),
             baseAddress: baseAddress,
+            bytesPerRow: UInt(self.bytesPerRow)
+        )
+    }
+}
+
+extension CGImage: GraphicsDataProvider {
+    public func graphicsData() throws -> GraphicsData {
+        guard let dataProvider = self.dataProvider,
+              let data = dataProvider.data,
+              let readOnlyBaseAddress = CFDataGetBytePtr(data)
+        else { throw GraphicsDataProviderError.missingData }
+        
+        let mutableBaseAddress = UnsafeMutablePointer<UInt8>(mutating: readOnlyBaseAddress)
+        
+        return .init(
+            width: UInt(self.width),
+            height: UInt(self.height),
+            baseAddress: mutableBaseAddress,
             bytesPerRow: UInt(self.bytesPerRow)
         )
     }
